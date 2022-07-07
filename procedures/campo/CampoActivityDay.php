aa051390e9b245963d5554165c5b29bf3aafb64b
DerianCordobaPerez <derianricardo451@gmail.com>
2022-07-07 16:47:17 -0600

    Add namespace to Campo procedures:

--- procedures/campo/CampoActivityDay.php
+++ procedures/campo/CampoActivityDay.php
@@ -1,38 +1,47 @@
 <?php
-include_once 'Connection.php';
-include_once 'utils/ToResponse.php';
+
+namespace Procedures\Campo;
+use App\Database\Connection;
+use App\Utils\ToResponse;
+
 class CampoActivityDay
 {
     use ToResponse;
+
     private Connection $connection;
 
     function __construct()
     {
         $this->connection = Connection::getInstance();
     }
+
     function get(): self
     {
-        $type = [
+        $types = [
             "CampoActividadDia",
             "CampoActividadDiaTotal",
             "CampoActividadDiaSubAct"
         ];
+
         $i = 0;
-        do{
+
+        do {
             $approve = $this->connection
                 ->parameters([
                     '@FechaInicial' => '2015-01-07 00:00:00',
                     '@FechaFinal' => '2016-06-30 00:00:00',
                     '@CodCiclo' => '001',
                     '@CodAct' => '002',
-                    '@Tipo' => $type[$i]
+                    '@Tipo' => $types[$i]
                 ])
                 ->exec('dbo.usp_Campo_Actividad_Dia')
                 ->fetch();
+
             $this->response($approve);
-            echo $this->toJson();
+
             $i++;
-        }while($i < count($type));
+        }while($i < count($types));
+
         return $this;
     }
 }
