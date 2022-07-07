9fa2477f1523a6a2908e5b68fb94c74f7481cdaa
MJackson22-bit <jm5444859@gmail.com>
2022-07-07 10:34:08 -0600

    feature CampoItemLand obtains data executing the stprage procedure dbo.usp_Campo_Rubro_Finca

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
     include_once 'Connection.php';
-    include_once 'procedures/campo/CampoItemFile.php';
+    include_once 'procedures/campo/CampoItemLand.php';
 
-    $activity = new CampoItemFile();
+    $activity = new CampoItemLand();
 
     $activity->get();
