17cff7e9808a4e6c487ff342f3469f1757858191
MJackson22-bit <jm5444859@gmail.com>
2022-07-07 09:05:50 -0600

    feature CampoLandItem obtains data executing the dtorage procedure dbo.usp_Campo_Finca_Lote

---
+++ procedures/campo/CampoLandItem.php
@@ -0,0 +1,43 @@
+<?php
+include_once 'Connection.php';
+include_once 'utils/ToResponse.php';
+class CampoLandItem
+{
+    private Connection $connection;
+
+    function __construct()
+    {
+        $this->connection = Connection::getInstance();
+    }
+    function get(): self
+    {
+        $type = [
+            "CampoFincaLote",
+            "CampoFincaLoteCuenta",
+            "CampoFincaLoteTop5",
+            "CampoFincaLoteArticulo"
+        ];
+        $i = 0;
+        do{
+            $approve = $this->connection
+                ->parameters([
+                    '@FechaInicial' => '2021-01-01 00:00:00',
+                    '@FechaFinal' => '2022-07-07 00:00:00',
+                    '@CodCiclo' => '018',
+                    '@CodRubro' => '00001',
+                    '@CodFinca' => '001',
+                    '@CodLote' => '018-HOR-01-01',
+                    '@Tipo' => $type[$i]
+                ])
+                ->exec('dbo.usp_Campo_Finca_Lote')
+                ->fetch();
+            $json = json_encode($approve, JSON_UNESCAPED_UNICODE);
+            if ($json)
+                echo $json;
+            else
+                echo json_last_error_msg();
+            $i++;
+        }while($i < count($type));
+        return $this;
+    }
+}
