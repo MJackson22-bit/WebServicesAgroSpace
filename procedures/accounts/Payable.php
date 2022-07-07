aa98ad686020ca46ed35035f90d926c1992bd3bc
DerianCordobaPerez <derianricardo451@gmail.com>
2022-07-07 17:38:47 -0600

    Add Maintenance and Accounts procedures

---
+++ procedures/accounts/Payable.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Procedures\Accounts;
+use App\Database\Connection;
+use App\Utils\ToResponse;
+
+class Payable
+{
+    use ToResponse;
+
+    private Connection $connection;
+
+    function __construct()
+    {
+        $this->connection = Connection::getInstance();
+    }
+
+    function get(): self
+    {
+        $approve = $this->connection
+            ->parameters([
+                '@FechaInicial' => '05-11-2016 00:00:00',
+                '@FechaFinal' => '05-11-2018 00:00:00',
+                '@CodEmp' => '001',
+                '@CodProv' => '0001',
+                '@Tipo' => "CxPPagos"
+            ])
+            ->exec('dbo.usp_CuentasxPagar_EstadoCuenta')
+            ->fetch();
+
+        $this->response($approve);
+
+        return $this;
+    }
+}
