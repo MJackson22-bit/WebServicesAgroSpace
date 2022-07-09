29a997e53b43e64ef162671385682a20dd37575a
DerianCordobaPerez <derianricardo451@gmail.com>
2022-07-09 09:12:37 -0600

    Uso del metodo index del controlador de inicio

--- app/controladores/ControladorInicio.php
+++ app/controladores/ControladorInicio.php
@@ -4,11 +4,6 @@ namespace App\Controladores;
 
 class ControladorInicio
 {
-    public function __construct()
-    {
-
-    }
-
     public function index(): void
     {
         echo require_once 'app/recursos/vistas/inicio.php';
--- app/enrutador/Rutas.php
+++ app/enrutador/Rutas.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Enrutador;
+use App\Controladores\ControladorInicio;
 use App\Singleton;
 use Inhere\Route\Router;
 use Throwable;
@@ -10,19 +11,21 @@ class Rutas
     use Singleton;
 
     private Router $router;
+    private ControladorInicio $controladorInicio;
 
-    /**
-     * @throws Throwable
-     */
     /**
      * La función __construct() es un método mágico que se llama cuando se crea una instancia de la clase. Crea una nueva
      * instancia de la clase Router y llama a la función rutas(). La función rutas() es donde definimos nuestras rutas. Se
      * llama a la función dispatch() en la instancia del enrutador y hace coincidir la solicitud actual con las rutas que
      * definimos en la función rutas()
+     *
+     * @throws Throwable
      */
     private function __construct()
     {
         $this->router = new Router();
+        $this->controladorInicio = new ControladorInicio();
+
         $this->rutas();
         $this->router->dispatch();
     }
@@ -32,7 +35,9 @@ class Rutas
      */
     private function rutas(): void
     {
-        $this->router->get('/', 'App\Controladores\ControladorInicio@index');
+        $this->router->get('/', function() {
+            $this->controladorInicio->index();
+        });
 
         $this->router->post('/data', function() {
             echo json_encode($_POST);
